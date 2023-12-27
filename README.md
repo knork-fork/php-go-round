82ce6ad512f8a09ae04530e1532308e598cba740
knork-fork <looka1998@gmail.com>
2023-12-27 22:21:01 +0100

    Add simple GO-PHP bridge

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # php-go-round
 
 PhpGoRound
+
+Start container:
+
+```
+docker-compose up --build -d
+```
+
+By default app should be available @ `localhost:50000`
+
+Or if already built:
+
+```
+docker-compose up -d
+```
+
+Stop container:
+
+```
+docker-compose down
+```
+
+Rebuild container:
+
+```
+docker-compose build
+```
+
+## Extras
+
+Enter the shell:
+
+```
+docker/shell
+```
---
+++ webserver/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "os/exec"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    // Construct the command to execute the PHP script
+    cmd := exec.Command("php", "/app/src/index.php")
+
+    // Execute the PHP script and capture its output.
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        // If there's an error executing the script, write the error to the response
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Write the output of the script to the HTTP response
+    w.Write(output)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
